7c94bb7108cd8a3e3f126de16d749a45f6805427
Alistair Quinn <alistairaldo@hotmail.co.uk>
2025-10-27 19:56:48 +0000

    Add project files.

---
+++ AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace Mauioke
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ GlobalXmlns.cs
@@ -0,0 +1,2 @@
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "Mauioke")]
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "Mauioke.Pages")]
